2ab42e369181605ccfde328a2831d92e75e1db20
Nate Lee <nleepercussion@gmail.com>
2021-09-18 16:53:28 -0400

    Added jobs delete route

--- models/db.js
+++ models/db.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_URI}`;
 let client;
 
 module.exports = async () => {
+
     try {
-      client = await MongoClient.connect(uri, { useNewUrlParser: true });
+      client = await MongoClient.connect(`mongodb+srv://${process.env.DB_URI}`, { useNewUrlParser: true });
     } catch (e) {
       console.log("Could not connect to mongodb");
     }
--- routes/jobs.js
+++ routes/jobs.js
@@ -20,13 +20,29 @@ router.get('/:user_id?', async (req, res) => {
 router.post('/add', async (req, res) => {
     const client = get();
     const jobs = client.db('grepJobs').collection('jobs');
-    const newJob = await jobs.insertOne(req.query, err => {
+    const newJob = await jobs.insertOne(req.body, err => {
         if (err) {
             console.error('ERROR', err)
         } else {
             console.log('insert successful')
         }
     });
-})
+});
+
+router.put('/update'), async (req, res) => {
+
+};
+
+router.delete('/delete', async (req, res) => {
+    const client = get();
+    const jobs = client.db('grepJobs').collection('jobs');
+    const response = await jobs.deleteOne(req.body, err => {
+        if (err) {
+            console.error('ERROR', err)
+        } else {
+            console.log('delete successful')
+        }
+    });
+});
 
 module.exports = router;
